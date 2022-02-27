d7d9c1f00d0330336221ac71b369277cba7b1dec
parksay_desktop <hellobigworld.parksay@gmail.com>
2022-02-27 19:27:49 +0900

    220227 queue first commit 1) created interface QueueInterface 2) created class QueueMain

---
+++ src/queue/QueueCircular.java
@@ -0,0 +1,30 @@
+package queue;
+
+public class QueueCircular implements QueueInterface {
+
+
+    public void queueInit() {
+
+    }
+
+    public boolean queueIsEmpty() {
+
+        return true;
+    }
+
+    public int enqueue(int data) {
+
+        return 0;
+    }
+
+    public int dequeue() {
+
+        return 0;
+    }
+
+    public int queuePeek() {
+
+        return 0;
+    }
+
+}
---
+++ src/queue/QueueMain.java
@@ -0,0 +1,20 @@
+package queue;
+
+public class QueueMain {
+
+
+    public static void main(String args[]) {
+        QueueCircular queue = new QueueCircular();
+        queue.queueInit();
+
+        int[] input = {3,7,9,13,5};
+        for(int i = 0; i < input.length; i++) {
+            queue.enqueue(input[i]);
+        }
+
+        while(!queue.queueIsEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+
+    }
+}
