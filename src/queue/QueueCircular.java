1af8f4a10efcd743a9c4d4ef3f8e6a3d6a3cf732
parksay_desktop <hellobigworld.parksay@gmail.com>
2022-02-27 20:10:44 +0900

    220227 1) created class QueueCircular

--- src/queue/QueueCircular.java
+++ src/queue/QueueCircular.java
@@ -3,28 +3,67 @@ package queue;
 public class QueueCircular implements QueueInterface {
 
 
+    private int head;
+    private int tail;
+    private int length;
+    private int[] dataArr;
+
     public void queueInit() {
+        this.head = 0;
+        this.tail = 0;
+        this.length = 10;
+        this.dataArr = new int[this.length];
 
     }
 
     public boolean queueIsEmpty() {
-
-        return true;
+        if(this.head == this.tail) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int enqueue(int data) {
+        if(this.queueIsFull()) {
+            return 0;
+        }
 
-        return 0;
+        this.tail = getNextPosition(this.tail);
+        this.dataArr[this.tail] = data;
+        return this.dataArr[this.tail];
     }
 
     public int dequeue() {
+        if(this.queueIsEmpty()) {
+            return 0;
+        }
 
-        return 0;
+        this.head = getNextPosition(this.head);
+        return this.dataArr[this.head];
     }
 
     public int queuePeek() {
+        if(this.queueIsEmpty()) {
+            return 0;
+        }
 
-        return 0;
+        return this.dataArr[this.head];
     }
 
+    private int getNextPosition(int cur) {
+        if(cur < this.length - 1) {
+            return ++cur;   //return cur++; occurs malfunctioning because it returns cur and then plus 1 after that
+        } else {
+            return 0;
+        }
+    }
+
+    private boolean queueIsFull() {
+        if(this.head == getNextPosition(this.tail)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/queue/QueueMain.java
+++ src/queue/QueueMain.java
@@ -6,6 +6,7 @@ public class QueueMain {
     public static void main(String args[]) {
         QueueCircular queue = new QueueCircular();
         queue.queueInit();
+        //TODO: try making QueueLinkedList
 
         int[] input = {3,7,9,13,5};
         for(int i = 0; i < input.length; i++) {
