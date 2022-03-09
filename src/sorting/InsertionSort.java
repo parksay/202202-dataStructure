41abbe67057063c73c57916aa1e58d266b1b354d
parksay_desktop <hellobigworld.parksay@gmail.com>
2022-03-10 00:42:58 +0900

    220309 1) created class InsertionSort : test passed

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -2,5 +2,68 @@ package sorting;
 
 public class InsertionSort {
 
+    public static boolean sortArray(int[] target) {
+        //divide the array into two area : sorted area and non-sorted area
+        //find the place for the next element from sorted area
+        //insert the next element into sorted area
 
+//            sorted area || non-sorted area
+//            O
+//        ||  2   1   5   4   3
+//                O
+//        2   ||  1   5   4   3
+//                    O
+//        1   2   ||  5   4   3
+//                        O
+//        1   2   5   ||  4   3
+//                            O
+//        1   2   4   5   ||  3
+//        1   2   3   4   5   ||
+
+
+
+        int length;
+        if(target == null) {
+            return false;
+        } else {
+            length = target.length;
+        }
+        if (length < 2) {
+            return false;
+        }
+
+        for(int i = 1; i < length; i++) {
+            for(int j = i; j > 0; j--) {
+                if(target[j] > target[j-1]) {
+                    break;
+                } else {
+                    int temp = target[j];
+                    target[j] = target[j-1];
+                    target[j-1] = temp;
+                    continue;
+                }
+            }
+        }
+
+        //original idea above, modified process below
+
+//        for(int i = 1; i < length; i++) {
+//            int cur = target[i];
+//            int index = i;
+//            for(int j = i; j > 0; j--) {
+//                if(cur > target[j-1]) {
+//                    target[index] = cur;
+//                    break;
+//                } else if(cur < target[j-1]) {
+//                    target[j] = target[j-1];
+//                    index = j-1;
+//                    continue;
+//                }
+//            }
+//            target[index] = cur;
+//
+//        }
+
+        return true;
+    }
 }
--- test/sort/InsertionSortTest.java
+++ test/sort/InsertionSortTest.java
@@ -1,36 +1,36 @@
 package sort;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import sorting.SelectionSort;
+import sorting.InsertionSort;
 
-public class SelectionSortTest {
+public class InsertionSortTest {
 
     @Test
     public void sortArrayTest() {
+
         //given
         int[] target = {1, 5, 2, 7, 4, 17, 84, 237, 52, 58, 89};
-        int[] err = null;
+        int[] err = {3};
 
         //when
-        SelectionSort.sortArray(target);
-        SelectionSort.sortArray(err);
-//        SelectionSort.originalIdea(target);
-//        SelectionSort.originalIdea(err);
-
+        InsertionSort.sortArray(target);
+        InsertionSort.sortArray(err);
 
         //then
         printArray(target);
-
+        printArray(err);
     }
+
 
     private void printArray(int[] target) {
         if(target == null) {
             return;
         }
 
-
         for(int i  = 0; i < target.length; i++) {
             System.out.println(target[i]);
         }
     }
+
 }
